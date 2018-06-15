2251f552b5a46c86d7dadfa851e770002c2b22ee
marianog <mariano.grimaux@gmail.com>
2018-06-14 23:39:32 -0300

    test2

--- src/tp/TPTest.java
+++ src/tp/TPTest.java
@@ -30,4 +30,24 @@ public class TPTest {
         System.out.println(tp.length);
         System.out.println(Arrays.toString(tp));
     }
+
+    public void test2() throws Exception {
+        final Elemento[][] elementos = new Elemento[4][4];
+        elementos[0][0] = new Elemento('B', 8);
+        elementos[0][1] = new Elemento('A', 3);
+        elementos[0][2] = new Elemento('A', 5);
+        elementos[0][3] = new Elemento('A', 9);
+        elementos[1][0] = new Elemento('B', 8);
+        elementos[1][1] = new Elemento('A', 5);
+        elementos[1][2] = new Elemento('B', 10);
+        elementos[1][3] = new Elemento('A', 20);
+        elementos[2][0] = new Elemento('A', 1);
+        elementos[2][1] = new Elemento('A', 1);
+        elementos[2][2] = new Elemento('A', 1);
+        elementos[2][3] = new Elemento('A', 1);
+        final Posicion[] tp = new TP().tp(elementos, new Posicion(1, 1), new Posicion(2, 3));
+
+        System.out.println(tp.length);
+        System.out.println(Arrays.toString(tp));
+    }
 }
